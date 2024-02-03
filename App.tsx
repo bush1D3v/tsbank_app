9df81b72f7eb6c2cb1a0437a92840d87702e1bff
NSJvictor <victornavarro@nasajon.com.br>
2024-02-02 22:54:42 -0300

    update(App.tsx): Reformulando estrutura padrao de projeto

--- App.tsx
+++ App.tsx
@@ -1,9 +1,7 @@
-import { Text, View } from "react-native";
+import Home from "./src/Home";
 
 export default function App(): React.JSX.Element {
   return (
-    <View className="p-20 flex-1 items-center justify-center">
-      <Text className="text-blue-700 font-bold text-3xl">Hello Nativewind</Text>
-    </View>
+    <Home />
   );
 }
