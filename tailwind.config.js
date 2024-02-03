bb5ea01d4368ec470b97a6b271c0d7f4a1d02e0c
NSJvictor <victornavarro@nasajon.com.br>
2024-02-02 22:57:31 -0300

    build(tailwind.config.js): Adicionando variaveis de cores ao framework

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./App.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueBase: "#3178c6",
+        darkBlue: "#1d4971",
+        lightBlue: "#4c8dcf",
+        saturatedBlue: "#0066b2",
+        desaturatedBlue: "#5394d4",
+        blackBlue: "#112069",
+        whiteBlue: "#2ec6e5",
+        purple: "#6b41d2",
+        black: "#000000",
+        white: "#eeeeee",
+        error: "#e70008",
+        input: "#f7e9fbb3",
+        darkGray: "#666666",
+        lightGray: "#cccccc",
+      },
+    },
   },
   plugins: [
     require("nativewind/tailwind/css"),
