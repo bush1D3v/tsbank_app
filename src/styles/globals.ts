ec400260258628ad1212e5d01d5570b409f55716
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-05 16:21:28 -0300

    feat(globals.ts): Cria arquivo de estilo global

---
+++ src/styles/globals.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const globals = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#3178c6",
+  },
+  text: {
+    color: "#eeeeee",
+    textAlign: "center",
+  }
+});
