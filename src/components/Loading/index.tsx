6e87132b62f52c1bfb3f77f159a518dd53b8c217
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-06 22:34:18 -0300

    update(index.tsx): Aumentando tamanho do componente de loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -4,7 +4,7 @@ import colors from "tailwindcss/colors";
 export function Loading(): React.JSX.Element {
   return (
     <View className="flex-1 justify-center items-center bg-blueBase">
-      <ActivityIndicator color={colors.white} />
+      <ActivityIndicator color={colors.white} size={"large"} />
     </View>
   );
 }
