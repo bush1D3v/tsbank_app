78d818906b83d35e2e390e79e27551c1b7a92661
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-06 22:09:10 -0300

    update(index.ts): Atualiza importacao de telas

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,6 +1,6 @@
 
-import LoginScreen from "./Login";
-import RegisterScreen from "./Register";
+import { LoginScreen } from "./Login";
+import { RegisterScreen } from "./Register";
 
 export {
   LoginScreen,
