a5fef3722f5d352ab44cff02068e6e8632ee4a53
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-12 09:16:07 -0300

    update(index.ts): Atualiza exportacao de componentes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,13 @@
 import { FormInput } from "./FormInput";
 import { Loading } from "./Loading";
+import { Modal } from "./Modal";
+import { Footer } from "./Footer";
+import { Header } from "./Header";
 
 export {
   FormInput,
-  Loading
+  Loading,
+  Modal,
+  Footer,
+  Header
 };
