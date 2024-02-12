337f2820e980df752a842eb3f909fefdce5b90fc
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-12 09:17:08 -0300

    feat(index.ts): Cria exportacao de componentes do Footer

---
+++ src/components/Footer/components/index.ts
@@ -0,0 +1,9 @@
+import { CopyrightText } from "./CopyrightText";
+import { DedicationText } from "./DedicationText";
+import { PolicyAndTerms } from "./PolicyAndTerms";
+
+export {
+  PolicyAndTerms,
+  DedicationText,
+  CopyrightText
+};
