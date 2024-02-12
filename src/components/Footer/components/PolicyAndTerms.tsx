8659434af520d980bf9546ebf0899aaa2b00826b
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-12 09:43:34 -0300

    (PolicyAndTerms.tsx): Cria componente de termos e politicas do Footer

---
+++ src/components/Footer/components/PolicyAndTerms.tsx
@@ -0,0 +1,35 @@
+import { globals } from "@/styles";
+import { Link } from "expo-router";
+import { FlatList } from "react-native";
+
+interface PolicyItem {
+  key: string;
+  href: `${string}:${string}`;
+}
+
+export function PolicyAndTerms(): React.JSX.Element {
+  const data: PolicyItem[] = [
+    {
+      key: "Privacy Policy",
+      href: "https://drive.google.com/file/d/1Mjdv1KKwJpRGx7gDOQ_X80mUAUpsSy7j/view?usp=sharing"
+    },
+    {
+      key: "Terms of use",
+      href: "https://drive.google.com/file/d/1YPhg5eVoAGnskfyPtwkGv9ch0qC7ZVyQ/view?usp=sharing"
+    }
+  ];
+
+  return (
+    <FlatList className="flex flex-col"
+      data={data}
+      renderItem={({ item }) =>
+        <Link
+          style={globals.text}
+          href={item.href}
+          className="underline"
+          rel="noreferrer noopener">
+          {item.key}
+        </Link>
+      } />
+  );
+}
