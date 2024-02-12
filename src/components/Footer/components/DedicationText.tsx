743bca52cc9fc7af2256f048e4dab2e19ebdcafb
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-12 09:43:07 -0300

    feat(DedicationText.tsx): Cria componente de texto de dedicacao do Footer

---
+++ src/components/Footer/components/DedicationText.tsx
@@ -0,0 +1,22 @@
+import { globals } from "@/styles";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export function DedicationText(): React.JSX.Element {
+  return (
+    <View className="flex flex-col text-center">
+      <Text style={globals.text}>
+        Made with 💖
+      </Text>
+      <Text style={globals.text}>
+        by&nbsp;
+        <Link style={globals.text}
+          className="font-bold underline"
+          href="https://bushi-links.vercel.app/"
+          rel="noreferrer noopener">
+          bush1D3v
+        </Link>
+      </Text>
+    </View>
+  );
+}
