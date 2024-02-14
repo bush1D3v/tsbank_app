ef6929fa111c805064481e7da3355054dc2d7d64
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-14 14:21:48 -0300

    update(routerPaths.ts): Atualiza rotas da aplicacao e adiciona tipagem dinamica

--- src/utils/routerPaths.ts
+++ src/utils/routerPaths.ts
@@ -1,2 +1,9 @@
-export const LOGIN: string = "/";
-export const REGISTER: string = "/register";
+import { Href } from "expo-router";
+
+type PathProp = Href<string>
+
+export const LOGIN: PathProp = "/";
+export const REGISTER: PathProp = "/register";
+export const HOME: PathProp = "/home";
+export const PROFILE: PathProp = "/profile/";
+export const ABOUT: PathProp = "/about";
