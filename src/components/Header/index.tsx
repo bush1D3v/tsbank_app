b7fc68b381cf862e5a03200c8378c8399230cdf9
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-14 13:50:43 -0300

    update(index.tsx): Atualiza layout do cabecalho

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,17 @@
-import { globals } from "@/styles";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import {
+  Logo,
+  ProfileIcon,
+  NavLinks
+} from "./components";
 
 export function Header(): React.JSX.Element {
   return (
-    <View className="bg-saturatedBlue flex items-center justify-center py-2
-    px-4 w-full h-14">
-      <Text style={globals.text}>Header</Text>
+    <View className="bg-saturatedBlue flex flex-row items-center
+    justify-between py-2 px-4 w-full h-16">
+      <Logo />
+      <NavLinks />
+      <ProfileIcon />
     </View>
   );
 }
