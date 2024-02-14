3c0dc730303fe05a851e10b9c9173caae2606636
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-14 14:14:12 -0300

    feat(LoginRedirect.tsx): Cria componente de LoginRedirect para o Registro

---
+++ src/screens/Register/components/LoginRedirect.tsx
@@ -0,0 +1,17 @@
+import { Link } from "expo-router";
+import { View, Text } from "react-native";
+import { globals } from "@/styles";
+import { LOGIN } from "@/utils/routerPaths";
+
+export function LoginRedirect(): React.JSX.Element {
+  return (
+    <View className="flex flex-row gap-2">
+      <Text style={globals.text} className="text-xl">
+        Already have an account?
+      </Text>
+      <Link style={globals.text} href={LOGIN} className="underline text-xl">
+        Sign In
+      </Link>
+    </View>
+  );
+}
