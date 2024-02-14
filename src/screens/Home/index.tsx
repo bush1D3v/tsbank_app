c894a2a782b143b47050b1200df20361915f0ff3
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-14 13:46:52 -0300

    update(index.tsx): Atualiza tela de Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,12 @@
-import { Footer, Header } from "@/components";
 import { globals } from "@/styles";
 import { View, Text } from "react-native";
 
 export function HomeScreen(): React.JSX.Element {
   return (
-    <View style={globals.container} className="justify-between">
-      <Header />
+    <View style={globals.container} className="justify-center">
       <Text style={globals.text}>
         Home
       </Text>
-      <Footer />
     </View>
   );
 }
