781941d5ab801a1c8837f185fae039219834d852
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-14 14:00:57 -0300

    update(index.tsx): Atualiza componente FormInput

--- src/components/FormInput/index.tsx
+++ src/components/FormInput/index.tsx
@@ -1,23 +1,108 @@
 import { TextInput, TextInputProps } from "react-native";
+import {
+  InputErrorHandling,
+  type FormStateData,
+  type UserDatas,
+  type CardDatas
+} from "./components";
+import {
+  type Control,
+  type FormState,
+  type UseFormClearErrors,
+  type UseFormGetFieldState,
+  type UseFormGetValues,
+  type UseFormHandleSubmit,
+  type UseFormRegister,
+  type UseFormReset,
+  type UseFormResetField,
+  type UseFormSetError,
+  type UseFormSetFocus,
+  type UseFormSetValue,
+  type UseFormTrigger,
+  type UseFormUnregister,
+  type UseFormWatch
+} from "react-hook-form";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+interface FormMethodsProps {
+  formState: FormState<FormStateData>;
+  setFocus: UseFormSetFocus<FormStateData>;
+  getFieldState: UseFormGetFieldState<FormStateData>;
+  handleSubmit: UseFormHandleSubmit<FormStateData>;
+  setValue: UseFormSetValue<any>;
+  trigger: UseFormTrigger<any>;
+  register: UseFormRegister<any>;
+  unregister: UseFormUnregister<any>;
+  clearErrors: UseFormClearErrors<any>;
+  watch: UseFormWatch<any>;
+  getValues: UseFormGetValues<any>;
+  reset: UseFormReset<any>;
+  resetField: UseFormResetField<any>;
+  setError: UseFormSetError<any>;
+  control: Control<any>;
+}
 
 type FormInputProps = TextInputProps & {
   placeholder: string;
-  ariaLabel: string;
+  inputLabel: UserDatas | CardDatas;
+  formMethods: FormMethodsProps;
 }
 
 export function FormInput({
   placeholder,
-  ariaLabel,
+  inputLabel,
+  formMethods,
   ...rest
 }: FormInputProps): React.JSX.Element {
+  function verifyLabelType(label: string): number {
+    //! 0 = UserDatas
+    //! 1 = CardDatas
+    if (
+      label === "id" || label === "name" || label === "email" ||
+      label === "password" || label === "phone" || label === "cpf" ||
+      label === "balance"
+    ) {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
+
   return (
-    <TextInput
-      className="h-10 w-full bg-white rounded-lg placeholder:pl-4 my-2
-      self-center border-2 border-white focus:border-blackBlue"
-      placeholder={placeholder}
-      aria-label={ariaLabel}
-      accessibilityLabel={ariaLabel}
-      {...rest}
-    />
+    <>
+      <InputErrorHandling
+        formState={formMethods.formState}
+        inputDatas={inputLabel}
+      />
+      {verifyLabelType(inputLabel) === 0 ? (
+        <TextInput
+          className="h-10 w-full bg-white rounded-lg placeholder:pl-4 my-2
+          self-center border-2 border-white focus:border-blackBlue"
+          placeholder={placeholder}
+          aria-label={placeholder}
+          accessibilityLabel={placeholder}
+          onChangeText={(text: string) => {
+            formMethods.setValue(`userData.${inputLabel}`, text);
+            formMethods.trigger(`userData.${inputLabel}`);
+          }}
+          {...formMethods.register(`userData.${inputLabel}`)}
+          {...rest}
+        />
+      ) : (
+        <TextInput
+          className="h-10 w-full bg-white rounded-lg placeholder:pl-4 my-2
+          self-center border-2 border-white focus:border-blackBlue"
+          placeholder={placeholder}
+          aria-label={placeholder}
+          accessibilityLabel={placeholder}
+          onChangeText={(text: string) => {
+            formMethods.setValue(`cardData.${inputLabel}`, text);
+            formMethods.trigger(`cardData.${inputLabel}`);
+          }}
+          {...formMethods.register(`cardData.${inputLabel}`)}
+          {...rest}
+        />
+      )}
+    </>
   );
 }
