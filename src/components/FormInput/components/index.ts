8ae537fe47e756fafb79ebad671cec580e3b536f
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-14 14:01:36 -0300

    feat(index.ts): Adiciona index de exportacao de componentes de FormInput

---
+++ src/components/FormInput/components/index.ts
@@ -0,0 +1,15 @@
+import {
+  InputErrorHandling,
+  type InputErrorHandlingProps,
+  type FormStateData,
+  type UserDatas,
+  type CardDatas
+} from "./InputErrorHandling";
+
+export {
+  InputErrorHandling,
+  type InputErrorHandlingProps,
+  type FormStateData,
+  type UserDatas,
+  type CardDatas
+};
