fae7dbcef02e73b07d7fb04d87b4b526bd4d6f44
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-15 16:24:26 -0300

    update(index.tsx): Adiciona tipagem de transacao ao input

--- src/components/FormInput/index.tsx
+++ src/components/FormInput/index.tsx
@@ -45,7 +45,7 @@ interface FormMethodsProps {
 
 type FormInputProps = TextInputProps & {
   placeholder: string;
-  inputLabel: UserDatas | CardDatas;
+  inputLabel: UserDatas | CardDatas | TransactionDatas;
   formMethods: FormMethodsProps;
 }
 
