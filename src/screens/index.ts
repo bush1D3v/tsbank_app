5c484bcaa458998365f8f771f1d90e9ef70847ac
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-21 19:25:45 -0300

    update: Atualiza index exportador de telas

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,14 +1,23 @@
-
 import { LoginScreen } from "./Login";
 import { RegisterScreen } from "./Register";
 import { HomeScreen } from "./Home";
 import { ProfileScreen } from "./Profile";
 import { AboutScreen } from "./About";
+import { CardScreen } from "./Card";
+import { PixScreen } from "./Pix";
+import { WithdrawScreen } from "./Withdraw";
+import { DepositScreen } from "./Deposit";
+import { ErrorScreen } from "./Error";
 
 export {
   LoginScreen,
   RegisterScreen,
   HomeScreen,
   ProfileScreen,
-  AboutScreen
+  AboutScreen,
+  CardScreen,
+  PixScreen,
+  WithdrawScreen,
+  DepositScreen,
+  ErrorScreen
 };
