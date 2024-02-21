c42869ce33c2796a9a3b1b50580e8c084168e214
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-21 19:24:11 -0300

    feat: Cria tela de home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,12 @@
 import { globals } from "@/styles";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { HooksList, UserReception } from "./components";
 
 export function HomeScreen(): React.JSX.Element {
   return (
     <View style={globals.container} className="justify-center">
-      <Text style={globals.text}>
-        Home
-      </Text>
+      <UserReception />
+      <HooksList />
     </View>
   );
 }
