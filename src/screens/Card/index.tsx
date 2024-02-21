3934d852c007f0aac884e0f7f1424413bfabdab7
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-21 19:23:51 -0300

    feat: Cria tela de cartao

---
+++ src/screens/Card/index.tsx
@@ -0,0 +1,10 @@
+import { globals } from "@/styles";
+import { View, Text } from "react-native";
+
+export function CardScreen(): React.JSX.Element {
+  return (
+    <View style={globals.container} className="justify-center">
+      <Text style={globals.text}>Card</Text>
+    </View>
+  );
+}
