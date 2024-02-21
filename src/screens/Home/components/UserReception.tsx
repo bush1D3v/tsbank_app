45d88998127b009c39f5c1f7f783df06febabb57
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-21 19:25:02 -0300

    feat: Cria componente para exibicao inicial de dados do usuario na home

---
+++ src/screens/Home/components/UserReception.tsx
@@ -0,0 +1,27 @@
+import { UserDataContext } from "@/contexts";
+import { balanceFormat } from "@/functions";
+import { globals } from "@/styles";
+import { useContext } from "react";
+import { Text, View } from "react-native";
+import Money from "@expo/vector-icons/FontAwesome";
+
+export function UserReception(): React.JSX.Element {
+  const { userData } = useContext(UserDataContext);
+
+  return (
+    <View className="flex items-center gap-4">
+      <Text style={globals.text} className="text-3xl font-bold -mb-2">
+        Hello
+      </Text>
+      <Text style={globals.text} className="text-2xl font-bold">
+        {userData.name} 👋
+      </Text>
+      <View className="flex flex-row items-center mb-4">
+        <Money name="money" size={25} color="white" />
+        <Text style={globals.text} className="text-2xl font-bold ml-3">
+          {balanceFormat(userData.balance)}
+        </Text>
+      </View>
+    </View>
+  );
+}
