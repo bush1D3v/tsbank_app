7b6a51c3e294a44948321b53ac10d4d54c8a5b6a
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-21 19:23:24 -0300

    update: Atualiza rotas do expo router

--- src/utils/routerPaths.ts
+++ src/utils/routerPaths.ts
@@ -7,3 +7,7 @@ export const REGISTER: PathProp = "/register";
 export const HOME: PathProp = "/home";
 export const PROFILE: PathProp = "/profile/";
 export const ABOUT: PathProp = "/about";
+export const WITHDRAW: PathProp = "/withdraw";
+export const DEPOSIT: PathProp = "/deposit";
+export const PIX: PathProp = "/pix";
+export const CARD: PathProp = "/card/";
