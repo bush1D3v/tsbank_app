5d470d176082e02d9fc92fbb3c1c420da387714d
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-21 19:22:19 -0300

    feat: Cria lista de botoes para transacoes diferentes

---
+++ src/screens/Home/components/HooksList.tsx
@@ -0,0 +1,49 @@
+import { AnchorLink } from "@/components";
+import { Href } from "expo-router";
+import { View } from "react-native";
+import {
+  DEPOSIT,
+  PIX,
+  WITHDRAW,
+  CARD
+} from "@/utils/routerPaths";
+
+interface HooksListProps {
+  key: string;
+  href: Href<string>;
+}
+
+export function HooksList(): React.JSX.Element {
+  const data: HooksListProps[] = [
+    {
+      key: "WITHDRAW",
+      href: WITHDRAW
+    },
+    {
+      key: "DEPOSIT",
+      href: DEPOSIT
+    },
+    {
+      key: "CARD",
+      href: CARD
+    },
+    {
+      key: "PIX",
+      href: PIX
+    }
+  ];
+
+  return (
+    <View>
+      {data.map((item, index) => (
+        <AnchorLink
+          key={index}
+          text={item.key}
+          buttonBg="bg-saturatedBlue"
+          className="text-white text-base"
+          href={item.href}
+        />
+      ))}
+    </View>
+  );
+}
