b2633ab059e299321f9d91c631683f484f8d1bef
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-21 19:23:07 -0300

    update: Atualiza rotas da api

--- src/utils/apiPaths.ts
+++ src/utils/apiPaths.ts
@@ -1,3 +1,7 @@
 export const API_BASE_URL: string = "https://tsbank-api.onrender.com/";
 export const LOGIN_ENDPOINT: string = "login";
 export const REGISTER_ENDPOINT: string = "user";
+export const WITHDRAW_ENDPOINT: string = "withdraw";
+export const DEPOSIT_ENDPOINT: string = "deposit";
+export const PIX_ENDPOINT: string = "pix";
+export const CARD_ENDPOINT: string = "card";
