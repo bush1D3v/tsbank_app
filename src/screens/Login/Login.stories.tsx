9038e191bcee8d5e852abbf533b64aa9069d97b8
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-24 23:59:59 -0300

    docs(Login.stories.tsx): Cria arquivo de documentacao do componente de Login

---
+++ src/screens/Login/Login.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { LoginScreen } from ".";
+
+const LoginMeta: Meta<typeof LoginScreen> = {
+  title: "Login",
+  component: LoginScreen
+};
+
+export default LoginMeta;
+
+export const Default: StoryObj<typeof LoginScreen> = {};
