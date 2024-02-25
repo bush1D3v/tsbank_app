022d98498578efa660d159c5480c279a73768eab
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-24 23:57:38 -0300

    update(LoginRedirect.tsx): Adicionando valores de testId aos elementos

--- src/screens/Register/components/LoginRedirect.tsx
+++ src/screens/Register/components/LoginRedirect.tsx
@@ -5,7 +5,7 @@ import { LOGIN } from "@/utils/routerPaths";
 
 export function LoginRedirect(): React.JSX.Element {
   return (
-    <View className="flex flex-row gap-2">
+    <View className="flex flex-row gap-2" testID="LoginRedirect">
       <Text style={globals.text} className="text-xl">
         Already have an account?
       </Text>
