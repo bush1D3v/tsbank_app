8090e325cce0343a09eff34c66c34b0090d4c661
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-25 00:00:11 -0300

    docs(Register.stories.tsx): Cria arquivo de documentacao do componente de Registro

---
+++ src/screens/Register/Register.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { RegisterScreen } from ".";
+
+const RegisterMeta: Meta<typeof RegisterScreen> = {
+  title: "Register",
+  component: RegisterScreen
+};
+
+export default RegisterMeta;
+
+export const Default: StoryObj<typeof RegisterScreen> = {};
