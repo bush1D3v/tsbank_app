35bbd7eb5318cfdc26c467d85b5beb2939d0446e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-24 23:55:37 -0300

    update(index.tsx): Adiciona parte inicializadora do Storybook

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,10 @@
+/*
+  / Para executar a documentação storybook, descomente a linha 6,
+  / e logo depois, comente todo o código que havia aqui existente.
+*/
+
+//* export { default } from "!/.storybook";
+
 import { LoginScreen } from "@/screens";
 
 export default function Login(): React.JSX.Element {
