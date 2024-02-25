c1252a6772c6ce1ef55a1d19877e8a9db2e8198d
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-24 23:47:08 -0300

    build(jest.config.js): Adiciona storybook ao ignorePatterns e cria bloco setupFilesAfterEnv

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   preset: "jest-expo",
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+    ".storybook",
   ],
-  setupFiles: ["./jestSetupFile.js"],
+  setupFilesAfterEnv: [
+    "@testing-library/jest-native/extend-expect",
+    "<rootDir>/jestSetupFile.js",
+  ],
 };
