d10072f17fc8893c26f7a93d82c2f1679ebdd3eb
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-03 20:07:25 -0300

    Atualiza exportacao de componentes do Header

--- src/components/Header/components/index.ts
+++ src/components/Header/components/index.ts
@@ -1,9 +1,7 @@
 import { Logo } from "./Logo";
 import { ProfileIcon } from "./ProfileIcon";
-import { NavLinks } from "./NavLinks";
 
 export {
   Logo,
-  ProfileIcon,
-  NavLinks
+  ProfileIcon
 };
