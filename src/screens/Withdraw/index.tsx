2882c7d642cf4d0e2e0227350af50a5730ee7d0f
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-03 20:05:46 -0300

    update: Adiciona valor de testID

--- src/screens/Withdraw/index.tsx
+++ src/screens/Withdraw/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 
 export function WithdrawScreen(): React.JSX.Element {
   return (
-    <View style={globals.container} className="justify-center">
+    <View style={globals.container} className="justify-center" testID="WithdrawScreen">
       <Text style={globals.text}>Withdraw</Text>
     </View>
   );
