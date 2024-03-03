5c94fe6d3b82a219192aa34354d16ef4f5674e7e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-03 20:04:57 -0300

    update: Adiciona valor de testID

--- src/screens/Deposit/index.tsx
+++ src/screens/Deposit/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 
 export function DepositScreen(): React.JSX.Element {
   return (
-    <View style={globals.container} className="justify-center">
+    <View style={globals.container} className="justify-center" testID="DepositScreen">
       <Text style={globals.text}>Deposit</Text>
     </View>
   );
