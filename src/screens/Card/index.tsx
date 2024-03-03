b9630a2ec6fad2801e5490cb13662c1d2204bf4e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-03 20:04:46 -0300

    update: Adiciona valor de testID

--- src/screens/Card/index.tsx
+++ src/screens/Card/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 
 export function CardScreen(): React.JSX.Element {
   return (
-    <View style={globals.container} className="justify-center">
+    <View style={globals.container} className="justify-center" testID="CardScreen">
       <Text style={globals.text}>Card</Text>
     </View>
   );
