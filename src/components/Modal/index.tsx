b6385fefad8ecbfe7e2a02d2a693756c3687d0b4
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-03 20:04:33 -0300

    update: Adiciona valores de testID

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -24,14 +24,19 @@ export function Modal({
     return (
       <View className="absolute flex top-0 bottom-0 left-0 right-0
       bg-[#00000090] justify-center z-10" testID="Modal">
-        <View className="bg-blueBase m-2 p-10 rounded-lg">
-          <Text style={globals.text} className="text-4xl mb-2 font-bold">
+        <View className="bg-blueBase m-2 p-10 rounded-lg" testID="ModalContent">
+          <Text style={globals.text} className="text-4xl mb-2 font-bold" testID="ModalTitle">
             {title}
           </Text>
-          <Text style={globals.text} className="mb-3 text-2xl">
+          <Text style={globals.text} className="mb-3 text-2xl" testID="ModalDescription">
             {description}
           </Text>
-          <Button title={btnMessage} onPress={() => { setOpen(!isOpen); }} />
+          <Button
+            title={btnMessage}
+            onPress={() => { setOpen(!isOpen); }}
+            color={globals.inputFormButton.color}
+            testID="ModalButton"
+          />
         </View>
       </View>
     );
