ecadea4da404ac86ab93d8d75e898008ff848dea
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-03 20:05:41 -0300

    update: Adiciona valor de testID

--- src/screens/Pix/index.tsx
+++ src/screens/Pix/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 
 export function PixScreen(): React.JSX.Element {
   return (
-    <View style={globals.container} className="justify-center">
+    <View style={globals.container} className="justify-center" testID="PixScreen">
       <Text style={globals.text}>Pix</Text>
     </View>
   );
