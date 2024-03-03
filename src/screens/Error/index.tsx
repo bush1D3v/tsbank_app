4e0d4d868503c0860620821a83e93f528ac17a78
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-03 20:06:04 -0300

    update: Adiciona valores de testID

--- src/screens/Error/index.tsx
+++ src/screens/Error/index.tsx
@@ -5,29 +5,27 @@ import { View, Text } from "react-native";
 
 export function ErrorScreen(): React.JSX.Element {
   return (
-    <View style={globals.container} className="justify-around text-center py-24">
-      <Text style={globals.text} className="text-4xl font-bold">
+    <View style={globals.container} testID="ErrorScreen"
+      className="justify-around text-center py-24">
+      <Text style={globals.text} testID="ErrorScreenTitle" className="text-4xl font-bold">
         Oops! 😯
       </Text>
-      <View className="flex p-4 gap-4">
-        <Text style={globals.text} className="text-2xl">
-          It seems like you got lost in the financial maze, but don't worry!
-          Our experts are working to resolve this. 👨‍💻 🛠️
-        </Text>
-        <Text style={globals.text} className="text-2xl">
-          In the meantime, check out our&nbsp;
-          <Text className="underline">amazing resources!</Text>&nbsp;
-          🚀 🌟
-        </Text >
-      </View>
-      <Text style={globals.text} className="text-6xl font-bold">
+      <Text style={globals.text} className="text-2xl p-4" testID="ErrorScreenText">
+        It seems like you got lost in the financial maze, but don't worry!
+        Our experts are working to resolve this. 👨‍💻 🛠️ {"\n"}
+        In the meantime, check out our&nbsp;
+        <Text className="underline">amazing resources!</Text>&nbsp;
+        🚀 🌟
+      </Text >
+      <Text style={globals.text} testID="ErrorScreenNumber" className="text-6xl font-bold">
         404
       </Text>
       <AnchorLink
         text="Return to home screen"
         href={HOME}
-        buttonBg="bg-saturatedBlue"
+        buttonBg="bg-blackBlue"
         className="py-3 px-5 text-white text-2xl"
+        testID="ErrorScreenButton"
       />
     </View>
   );
