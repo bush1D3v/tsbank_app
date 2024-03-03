f4e70245335fedb1cde36ff22d32fbb39cb44917
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-03 20:03:19 -0300

    update: Adiciona valor de testID

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,7 +8,7 @@ import {
 export function Footer(): React.JSX.Element {
   return (
     <View className="text-sm w-full bg-saturatedBlue flex flex-row
-    justify-between py-2 px-4 relative h-14">
+    justify-between py-2 px-4 relative h-14" testID="Footer">
       <CopyrightText />
       <PolicyAndTerms />
       <DedicationText />
