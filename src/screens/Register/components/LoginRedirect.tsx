f359e40a856c681688de3299b1bced59c38a1642
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-03 20:05:57 -0300

    update: Adiciona valores de testID

--- src/screens/Register/components/LoginRedirect.tsx
+++ src/screens/Register/components/LoginRedirect.tsx
@@ -6,10 +6,11 @@ import { LOGIN } from "@/utils/routerPaths";
 export function LoginRedirect(): React.JSX.Element {
   return (
     <View className="flex flex-row gap-2" testID="LoginRedirect">
-      <Text style={globals.text} className="text-xl">
+      <Text style={globals.text} className="text-xl" testID="LoginRedirectText">
         Already have an account?
       </Text>
-      <Link style={globals.text} href={LOGIN} className="underline text-xl">
+      <Link style={globals.text} href={LOGIN} testID="LoginRedirectLink"
+        className="underline text-xl">
         Sign In
       </Link>
     </View>
