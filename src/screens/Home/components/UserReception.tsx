b244dee281d0360f0fcdec61972e2c5d04398f81
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-03 20:06:11 -0300

    update: Adiciona valores de testID

--- src/screens/Home/components/UserReception.tsx
+++ src/screens/Home/components/UserReception.tsx
@@ -9,14 +9,16 @@ export function UserReception(): React.JSX.Element {
   const { userData } = useContext(UserDataContext);
 
   return (
-    <View className="flex items-center gap-4">
-      <Text style={globals.text} className="text-3xl font-bold">
-        Hello {"\n"}
+    <View className="flex items-center gap-4" testID="UserReception">
+      <Text style={globals.text} className="text-3xl font-bold -mb-2" testID="UserReceptionHello">
+        Hello
+      </Text>
+      <Text style={globals.text} className="text-2xl font-bold" testID="UserReceptionName">
         {userData.name} 👋
       </Text>
-      <View className="flex flex-row items-center mb-4">
-        <Money name="money" size={25} color="white" />
-        <Text style={globals.text} className="text-2xl font-bold ml-3">
+      <View className="flex flex-row items-center mb-4" testID="UserReceptionBalanceDivision">
+        <Money name="money" size={25} color="white" testID="UserReceptionBalanceIcon" />
+        <Text style={globals.text} className="text-2xl font-bold ml-3" testID="UserReceptionBalanceValue">
           {balanceFormat(userData.balance)}
         </Text>
       </View>
