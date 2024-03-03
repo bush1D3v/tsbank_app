fa865494e74ac9a45d9300cb515457bb2ecfa5f5
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-03 20:04:25 -0300

    update: Adiciona valores de testID e links de navegacao

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,20 @@
 import { View } from "react-native";
 import {
   Logo,
-  ProfileIcon,
-  NavLinks
+  ProfileIcon
 } from "./components";
+import { AnchorLink } from "../AnchorLink";
+import { ABOUT, HOME } from "@/utils/routerPaths";
 
 export function Header(): React.JSX.Element {
   return (
     <View className="bg-saturatedBlue flex flex-row items-center
-    justify-between py-2 px-4 w-full h-16">
+    justify-between py-2 px-4 w-full h-16" testID="Header">
       <Logo />
-      <NavLinks />
+      <View className="flex justify-center items-center flex-row">
+        <AnchorLink text="Home" href={HOME} buttonBg="bg-blueBase" />
+        <AnchorLink text="About" href={ABOUT} buttonBg="bg-blueBase" />
+      </View>
       <ProfileIcon />
     </View>
   );
