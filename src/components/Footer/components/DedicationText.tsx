0bf1a77bdecefc39a36dd949f65dd27a75817ff4
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-03 20:03:29 -0300

    update: Adiciona valores de testID

--- src/components/Footer/components/DedicationText.tsx
+++ src/components/Footer/components/DedicationText.tsx
@@ -4,16 +4,17 @@ import { Text, View } from "react-native";
 
 export function DedicationText(): React.JSX.Element {
   return (
-    <View className="flex text-center">
-      <Text style={globals.text}>
+    <View className="flex text-center" testID="DedicationText">
+      <Text style={globals.text} testID="DedicationTextMade">
         Made with 💖
       </Text>
-      <Text style={globals.text}>
+      <Text style={globals.text} testID="DedicationTextBy">
         by&nbsp;
-        <Link style={globals.text}
+        <Link
           className="font-bold underline"
           href="https://bushi-links.vercel.app/"
-          rel="noreferrer noopener">
+          rel="noreferrer noopener"
+          testID="DedicationTextLink">
           bush1D3v
         </Link>
       </Text>
