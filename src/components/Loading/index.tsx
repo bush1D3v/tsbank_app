42c4af1cd81e4b615959b29543accf24ce9da72e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-03 20:05:21 -0300

    update: Adiciona valores de testID

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,8 +3,8 @@ import colors from "tailwindcss/colors";
 
 export function Loading(): React.JSX.Element {
   return (
-    <View className="flex-1 justify-center items-center bg-blueBase">
-      <ActivityIndicator color={colors.white} size={"large"} />
+    <View className="flex-1 justify-center items-center bg-blueBase" testID="Loading">
+      <ActivityIndicator color={colors.white} size={"large"} testID="LoadingIndicator" />
     </View>
   );
 }
